d79f83108217b9913030aac388ea7d4774c05539
Senuka Chandunu <senukaacc@gmail.com>
2025-07-16 22:22:45 +0530

    update footer & header components in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,17 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Inter, Playfair_Display } from 'next/font/google'
+
+import { cn } from '@/lib/utils'
+
 import './globals.css'
 import Providers from '@/components/providers'
+import Header from '@/components/header'
+import Footer from '@/components/footer'
 
-const inter = Inter({
-  subsets: ['latin']
+const inter = Inter({ subsets: ['latin'], variable: '--font-sans' })
+const playfair = Playfair_Display({
+  subsets: ['latin'],
+  variable: '--font-serif'
 })
 
 export const metadata: Metadata = {
@@ -18,9 +25,19 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={`${inter.className} antialiased`}>
-        <Providers>{children}</Providers>
+    <html lang='en' suppressHydrationWarning>
+      <body
+        className={cn(
+          'flex min-h-screen flex-col font-sans antialiased',
+          inter.variable,
+          playfair.variable
+        )}
+      >
+        <Providers>
+          <Header />
+          <main className='grow'>{children}</main>
+          <Footer />
+        </Providers>
       </body>
     </html>
   )
