3379ceb83df623bedffc24468401c894b3448fba
Senuka Chandunu <senukaacc@gmail.com>
2025-07-16 23:23:52 +0530

    feat: create base hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Button } from '@/components/ui/button'
-// import  Footer from '@/components/footer'
+import Intro from '@/components/hero'
 
 export default function Home() {
   return (
       <section className="py-24">
         <div className="container max-w-3xl">
-          <h1 className="text-3xl font-bold">Portfolio</h1>
+          <Intro/>
         </div>
       </section>
   )
---
+++ src/components/hero.tsx
@@ -0,0 +1,26 @@
+import Image from 'next/image'
+
+export default function Intro() {
+  return (
+    <section className='flex flex-col-reverse items-start gap-x-10 gap-y-4 pb-24 md:flex-row md:items-center'>
+      <div className='mt-2 flex-1 md:mt-0'>
+        <h1 className='title no-underline'>Hey, I&#39;m Senuka.</h1>
+        <p className='mt-3 font-light text-muted-foreground'>
+          I&#39;m a software engineer based in Colombo, Sri Lanka. I&#39;m
+          passionate about learning new technologies and sharing knowledge with
+          others.
+        </p>
+      </div>
+      <div className='relative'>
+        <Image
+          src="/assets/images/IMG_5360.png"
+          className='flex-1 rounded-lg grayscale'
+          alt='Senuka Chandunu'
+          width={175}
+          height={175}
+          priority
+        />
+      </div>
+    </section>
+  )
+}
