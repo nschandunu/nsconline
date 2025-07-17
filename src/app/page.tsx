51d7796104950e85442bdb56873392bcd0e2c0b0
Senuka Chandunu <senukaacc@gmail.com>
2025-07-17 19:24:22 +0530

    install MDX markdown package and render demo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
-import { Button } from '@/components/ui/button'
 import Intro from '@/components/hero'
-
+import { MDXRemote } from 'next-mdx-remote/rsc'
+ 
 export default function Home() {
+  const content = `
+    #This is a markdown heading
+  `
+
   return (
       <section className="py-24">
         <div className="container max-w-3xl">
           <Intro/>
+
+          <MDXRemote source={content}/>
         </div>
       </section>
   )
