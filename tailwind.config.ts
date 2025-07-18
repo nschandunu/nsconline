cca13c3f90621232004dfe2e3a545216a7ca5424
Senuka Chandunu <senukaacc@gmail.com>
2025-07-18 13:03:48 +0530

    dark | light theme toggle to footer | padding changes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,23 +8,38 @@ const config: Config = {
   theme: {
     container: {
         center: true,
-        padding: '1rem',
+        padding: '4rem',
+
+        // padding: {
+        //   DEFAULT: '1rem',
+        //   mac14: '2rem',
+        //   '2xl': '4rem',
+        // },
         
         screens: {
-            'iphone-x': '375px',        // iPhone SE, iPhone 6/7/8, iPhone X
-            'iphone-12-14': '390px',     // iPhone 12/13/14
-            'iphone-pro': '393px',     // iPhone 14 Pro
-            'iphone-promax': '430px',  // iPhone 14 Pro Max / 15 Pro Max
             sm: '640px',       // Mobile
             md: '768px',       // Tablet
             lg: '1024px',      // Small laptops
             xl: '1280px',      // Medium laptops / 13" MacBook Air
-            '2xl': '1440px',   // 15.4" laptops (Full HD, non-Retina) / 15" MacBook Air (Retina)
             'mac14': '1512px', // 14" MacBook Pro (Retina)
-            'mac16': '1728px', // 16" MacBook Pro (Retina)
-            '4xl': '1920px',   // Full HD desktop monitors
-            '5xl': '2560px',   // 2K / Retina displays
+            '2xl': '1536px',   // 15.4" laptops (Full HD, non-Retina)
           }
+
+          // screens: {
+          //   'iphone-x': '375px',        // iPhone SE, iPhone 6/7/8, iPhone X
+          //   'iphone-12-14': '390px',     // iPhone 12/13/14
+          //   'iphone-pro': '393px',     // iPhone 14 Pro
+          //   'iphone-promax': '430px',  // iPhone 14 Pro Max / 15 Pro Max
+          //   sm: '640px',       // Mobile
+          //   md: '768px',       // Tablet
+          //   lg: '1024px',      // Small laptops
+          //   xl: '1280px',      // Medium laptops / 13" MacBook Air
+          //   '2xl': '1440px',   // 15.4" laptops (Full HD, non-Retina) / 15" MacBook Air (Retina)
+          //   'mac14': '1512px', // 14" MacBook Pro (Retina)
+          //   'mac16': '1728px', // 16" MacBook Pro (Retina)
+          //   '4xl': '1920px',   // Full HD desktop monitors
+          //   '5xl': '2560px',   // 2K / Retina displays
+          // }
     },
     extend: {
         colors: {
