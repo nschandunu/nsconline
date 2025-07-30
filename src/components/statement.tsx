f48ac858322643bf512ac6d75a4515d8ae0a8ee6
Senuka Chandunu <senukaacc@gmail.com>
2025-07-30 17:41:16 +0530

    feat: txt size

--- src/components/statement.tsx
+++ src/components/statement.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Statement = () => {
   return (
     <div className="mt-56 mb-36 flex items-center justify-center font-sans">
-      <h1 className="animate-intro-gradient bg-[linear-gradient(91.36deg,#eca658,#f391a6_13.02%,#e188c3_25.52%,#a58de3_37.5%,#56abec_49.48%,#737eb7_63.02%,#c8638c_72.92%,#dd5d57_84.38%,#df6c51_97.92%)] bg-[length:200%_200%] bg-clip-text text-center text-[92px] leading-[1.05] font-semibold tracking-[-0.045em] text-transparent">
+      <h1 className="animate-intro-gradient bg-[linear-gradient(91.36deg,#eca658,#f391a6_13.02%,#e188c3_25.52%,#a58de3_37.5%,#56abec_49.48%,#737eb7_63.02%,#c8638c_72.92%,#dd5d57_84.38%,#df6c51_97.92%)] bg-[length:200%_200%] bg-clip-text text-center text-[104px] leading-[1.05] font-semibold tracking-[-0.045em] text-transparent">
         World-class Digital
         <br />
         stuff that actually
